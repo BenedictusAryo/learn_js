99d572bcb847987311d3d64eda505a838a652b44
BenedictusAryo <benedictusaryo@outlook.com>
2020-11-28 21:52:18 +0700

    Primitive types:

---
+++ Basics/3_Basics/index.js
@@ -0,0 +1,22 @@
+//  Variable
+let name = 'Ben';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are Case-sensitive
+
+// Constant
+const interestRate = 0.3;
+console.log(interestRate);
+
+// --------------------------------------
+// -- PRIMITIVE TYPES --
+let nickName = 'Ben'; //String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName; 
+let firsName = undefined;
+let selectedColor = null;
