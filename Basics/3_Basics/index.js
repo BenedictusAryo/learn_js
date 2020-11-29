e519f9e3828afdad932cc61191818fbf38683bc8
BenedictusAryo <benedictusaryo@outlook.com>
2020-11-29 19:27:08 +0700

    Operators

--- Basics/3_Basics/index.js
+++ Basics/3_Basics/index.js
@@ -21,8 +21,79 @@ let firstName;
 let firsName = undefined;
 let selectedColor = null;
 
-console.log(typeof nickName);
 console.log(typeof age);
+console.log(typeof nickName);
 console.log(typeof isApproved);
 console.log(typeof firsName);
 console.log(typeof selectedColor);
+
+// --------------------------------------
+// -- REFERENCE TYPES --
+
+// Object
+let person = {
+    name: 'Ben',
+    age: 30
+};
+
+console.log(person);
+console.log(typeof person);
+
+// Dot Notation 
+person.name = 'John';
+console.log(person.name);
+
+// Bracket Notation 
+person['name'] = 'Mary';
+console.log(person['name']);
+
+// Use case of Bracket Notation 
+let selection = 'name';
+person[selection] = 'Mary'
+
+// Array
+let selectedColors = ['red','blue'];
+console.log(selectedColors)
+console.log(typeof selectedColors);
+// Display First Element Array
+console.log(selectedColors[0]);
+
+// Add Element to array
+selectedColors[2] = 'green';
+console.log(selectedColors);
+
+// Multi types on array
+selectedColors[3] = 3;
+console.log(selectedColors);
+
+// Object properties
+console.log(selectedColors.length);
+
+
+// Function
+function greet() {
+    console.log('Hello World');
+}
+
+greet();
+
+// Function with parameter
+function greet(name) {
+    console.log('Hello ' + name);
+}
+// With Argument
+greet('Ben');
+
+// Multiple parameters that Performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+greet('John', 'Smith'); 
+
+// Function calculating a value
+function square (number) {
+    return number * number;
+}
+let result = square(4);
+console.log(result);
+console.log(square(5));
---
+++ Basics/4_Operators/index.js
@@ -0,0 +1,23 @@
+// Operators
+
+
+
+// Arithmetics Operators
+let x = 10;
+let y = 3
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+// Increment (++) 
+console.log(++x); // Increment by 1 then print to console
+console.log(x++); //Print to the console and then add 1
+console.log(x);
+
+// Decrement (--)
+console.log(--x); //Decrement by 1 then print to console
+console.log(x--); // Print to console then minus 1
+console.log(x); 
